f2686a35943efd163da1b9287698f65abdd22eba
Kaveesha Himasanka <kaveee143@gmail.com>
2022-08-28 12:20:51 +0530

    set time convert method

--- app/src/main/java/com/example/musicapp/MusicPlayerActivity.java
+++ app/src/main/java/com/example/musicapp/MusicPlayerActivity.java
@@ -1,13 +1,69 @@
 package com.example.musicapp;
 
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class MusicPlayerActivity extends AppCompatActivity {
+
+    TextView titleTv, currentTimeTv, totalTimeTv;
+    SeekBar seekBar;
+    ImageView pausePlay, nextBtn, previousBtn, musicIcon;
+    ArrayList<AudioModel> songsList;
+    AudioModel currentSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        titleTv = findViewById(R.id.song_title);
+        currentTimeTv = findViewById(R.id.current_time);
+        totalTimeTv = findViewById(R.id.total_time);
+        seekBar = findViewById(R.id.seek_bar);
+        pausePlay = findViewById(R.id.pause_play);
+        nextBtn = findViewById(R.id.next);
+        previousBtn = findViewById(R.id.previous);
+        musicIcon = findViewById(R.id.music_icon_big);
+
+        songsList = (ArrayList<AudioModel>) getIntent().getSerializableExtra("LIST");
+
+        setResourcesWithMusic();
+    }
+
+    void setResourcesWithMusic(){
+        currentSong = songsList.get(MyMediaPlayer.currentIndex);
+        titleTv.setText(currentSong.getTitle());
+
+        totalTimeTv.setText(convertToMMSS(currentSong.getDuration()));
+    }
+
+    private void playMusic(){
+
+    }
+
+    private void playNextSong(){
+
+    }
+
+    private void playPreviousSong(){
+
+    }
+
+    private void pausePlay(){
+
+    }
+
+    public static String convertToMMSS(String duration){
+        Long millis = Long.parseLong(duration);
+        return String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.HOURS.toSeconds(1));
     }
 }
